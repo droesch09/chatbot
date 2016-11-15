1efb9af0c1fd9f9fe209559ad92efe39299e3d9f
Daniel Rösch <danielrosch@daniels-mbp-2.speedportneo09012801000249>
2016-11-15 21:11:50 +0100

    Third Commit

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-echo "Hello World";
+
+if(isset($_REQUEST['hub_challenge'])) {
+	$challenge = $_REQUEST['hub_challenge'];
+	$token = $_REQUEST['hub_verify_token']; 
+}
+
+if($token== "mrcareermr") {
+	echo $challenge;
+}
+
 ?>
