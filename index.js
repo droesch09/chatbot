8c29b52c5894e2c1e87241db7c4c6416154fad6a
Daniel Rösch <danielrosch@daniels-mbp-2.speedportneo09012801000249>
2016-11-19 23:05:51 +0100

    verify token set

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.post('/webhook/', function (req, res) {
     res.sendStatus(200)
 })
 
-var token = "<my_voice_is_my_password_verify_me>"
+var token = "<"EAAFKotdMmt4BAJcULNzApVqqnhfvDP0TgmCXcGUtZCEhXWjnUnHGPNgAWJuOitz4Klt4dnCqR4e3egxv4jYJuWE3LZCn7T5fne5SBvYtgYyutFm6lqIgu0UKdZAJpkWWawB5UhGIq29rqzZA6f9zApNtwqZAR51uHGfaKpHDfkgZDZD“>"
 
 
 function sendTextMessage(sender, text) {
