36c2bd194e0f26c200af6ff50680118b43d40476
Daniel Rösch <danielrosch@daniels-mbp-2.speedportneo09012801000249>
2016-11-20 08:04:31 +0100

    verify token set

--- index.js
+++ index.js
@@ -33,6 +33,10 @@ app.post('/webhook/', function (req, res) {
         sender = event.sender.id
         if (event.message && event.message.text) {
             text = event.message.text
+            if (text === 'Hello' || text === 'Hi' || text === 'Hey' || text === 'hello' || text === 'hi' || text === 'hey') {
+                sendGenericMessage(sender)
+                continue
+            }
             sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
         }
     }
@@ -62,6 +66,27 @@ function sendTextMessage(sender, text) {
     })
 }
 
+
+function sendGenericMessage(sender) {
+    messageData = {
+        text:"Hallo, ich bin Mr. Career und arbeite für Daniel Rösch.\nich kann für dich folgende Dinge erledigen\n1. blablabla\2. blablabla"
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
 
 // Spin up the server
 app.listen(app.get('port'), function() {
