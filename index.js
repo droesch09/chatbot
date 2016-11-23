5c467370265bf14fb644e945da2d57d81c0bee5e
Daniel Rösch <danielrosch@daniels-mbp-2.speedportneo09012801000249>
2016-11-23 20:16:14 +0100

    no Message

--- index.js
+++ index.js
@@ -311,7 +311,7 @@ function sendDanielMessage(sender) {
 }
 
 function getPorscheURL(entryLevel, activityLevel){
-    String URL "https://jobs.porsche.com/index.php?ac=search_result&ac=search_result&search_parameter_page_current=1&hitsperpage=10&search_parameter_order_criterion=date&search_parameter_order_direction=DESC&search_criterion_keyword=&search_criterion_activity_level=" + activityLevel + "&search_criterion_entry_level=" + entryLevel + "&search_criterion_country=&search_criterion_physical_location=&search_criterion_division=&btn_dosearch=1000+Treffer+#search-options"
+    var URL "https://jobs.porsche.com/index.php?ac=search_result&ac=search_result&search_parameter_page_current=1&hitsperpage=10&search_parameter_order_criterion=date&search_parameter_order_direction=DESC&search_criterion_keyword=&search_criterion_activity_level=" + activityLevel + "&search_criterion_entry_level=" + entryLevel + "&search_criterion_country=&search_criterion_physical_location=&search_criterion_division=&btn_dosearch=1000+Treffer+#search-options"
     
     return URL;
 }
